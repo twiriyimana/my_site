515c1b3a46f43c40ac293f182f86840e02c7d606
twiriyimana <jeanchriso394@gmail.com>
2025-11-27 14:10:34 +0200

    final fix

--- src/Pages/Heads.jsx
+++ src/Pages/Heads.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Heads() {
   return (
@@ -9,9 +10,9 @@ function Heads() {
    <img src="1.jpg" className="rounded-full h-15 w-15"/>
     </div>
    <ul className="flex">
-   <a href="/"> <li className="p-3">Home</li></a>
-    <a href="./about"><li className="p-3">About</li></a>
-<a href="/contact"><li className="p-3">Address</li></a>
+   <Link to="/"> <li className="p-3">Home</li></Link>
+    <Link to="./about"><li className="p-3">About</li></Link>
+<Link to="/contact"><li className="p-3">Address</li></Link>
    </ul>
 <button className="bg-blue-600 text-white px-5 md:px-6 py-2 rounded-full font-semibold shadow hover:bg-blue-700 transition w-full md:w-auto">
           Hire Me &rarr;
