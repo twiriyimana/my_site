471dc6e061a1159dfb2aa6dcfac7e8a024658c1a
twiriyimana <jeanchriso394@gmail.com>
2025-11-27 14:18:56 +0200

    upd: absolute final fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
 function Home() {
@@ -10,9 +11,9 @@ function Home() {
    <img src="1.jpg" className="rounded-full h-15 w-15"/>
     </div>
    <ul className="flex">
-   <a href=""> <li className="p-3">Home</li></a>
-    <a href="/about"><li className="p-3">About</li></a>
-<a href="/contact"><li className="p-3">Address</li></a>
+   <Link to=""> <li className="p-3">Home</li></Link>
+    <Link to="/about"><li className="p-3">About</li></Link>
+<Link to="/contact"><li className="p-3">Address</li></Link>
    </ul>
 <button className="bg-blue-600 text-white px-5 md:px-6 py-2 rounded-full font-semibold shadow hover:bg-blue-700 transition w-full md:w-auto">
           Hire Me &rarr;
